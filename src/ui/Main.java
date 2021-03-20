70f3d321aeb1d0807d1a30d2ee18f5121df74bc1
santiagoarevalo <santiguitar2003@gmail.com>
2021-03-20 16:40:20 -0500

    Main class implemented

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,10 +1,35 @@
 package ui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import model.Restaurant;
 
+public class Main extends Application {
+	private WelcomeGUI welcomeGUI;
+	private Restaurant restaurant;
+
+	public Main(){
+		restaurant = new Restaurant();
+		welcomeGUI = new WelcomeGUI(restaurant);
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		launch(args);
 	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("welcome.fxml"));
+
+		fxmlLoader.setController(welcomeGUI);
+		
+		Parent root = fxmlLoader.load();
+		
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Casa Dorada Restaurant");
+		primaryStage.show();
+	}
 }
