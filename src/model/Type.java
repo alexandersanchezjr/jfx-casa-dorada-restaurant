00fa97b2e068fe64daf8dcc1c4e581b4caa99e30
santiagoarevalo <santiguitar2003@gmail.com>
2021-03-19 23:17:10 -0500

    Type class implemented

--- src/model/Type.java
+++ src/model/Type.java
@@ -2,14 +2,12 @@ package model;
 
 public class Type {
 	private String name;
-	private boolean availability;
 	/**
 	 * @param name
 	 * @param availability
 	 */
-	public Type(String name, boolean availability) {
+	public Type(String name) {
 		this.name = name;
-		this.availability = availability;
 	}
 	/**
 	 * @return the name
@@ -22,17 +20,5 @@ public class Type {
 	 */
 	public void setName(String name) {
 		this.name = name;
-	}
-	/**
-	 * @return the availability
-	 */
-	public boolean isAvailability() {
-		return availability;
-	}
-	/**
-	 * @param availability the availability to set
-	 */
-	public void setAvailability(boolean availability) {
-		this.availability = availability;
 	}
 }
