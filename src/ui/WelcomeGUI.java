7ab696c66b8e14a6e24ece7d197be7d9123f4904
santiagoarevalo <santiguitar2003@gmail.com>
2021-03-21 19:21:47 -0500

    Principal Controller class created

--- src/ui/WelcomeGUI.java
+++ src/ui/WelcomeGUI.java
@@ -6,68 +6,83 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 import model.Restaurant;
 
 public class WelcomeGUI {
 	
 	@FXML
-    private AnchorPane adminPane;
+    private AnchorPane mainAnchorPane;
 
     @FXML
-    private TextField adminUserTxt;
+    private TextField usernameTxt;
 
     @FXML
-    private PasswordField adminPasswordTxt;
+    private PasswordField userPasswordTxt;
 
-    @FXML
-    private AnchorPane employeePane;
-
-    @FXML
-    private TextField employeeUserTxt;
-
-    @FXML
-    private TextField employeeIdTxt;
-	
+    
 	private Restaurant restaurant;
+	private EmployeeGUI employeeGUI;
+	private AdminGUI adminGUI;
     
     public WelcomeGUI(Restaurant restaurant) {
     	this.restaurant = restaurant;
+    	employeeGUI = new EmployeeGUI();
+    	adminGUI = new AdminGUI();
     }
   
 	public void initialize() {
     	//the method (initialize) is called several times by diferents fxml file loads 
     }
-    
+
     @FXML
-    public void loadLogIn(ActionEvent event) throws IOException {
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login.fxml"));
+    public void showAdminWindow(ActionEvent event) throws IOException {
+    	/*boolean logged = false;
 		
-		fxmlLoader.setController(this);    	
-		Parent login = fxmlLoader.load();
- 
-    }
-    
+		if(restaurant.getEmployees().size() == 0) {
+    		Alert alert = new Alert(AlertType.ERROR);
+	    	alert.setTitle("Log In incorrect");
+	    	alert.setHeaderText(null);
+	    	alert.setContentText("The username or password given are incorrect");
+	    	alert.showAndWait();
+	    	employeeUserTxt.setText(""); 
+	    	employeeIdTxt.setText("");
+    	}
+    	else {
+    		for(int i=0; i<restaurant.getEmployees().size(); i++) {
+        		if(employeeUserTxt.getText().equals(restaurant.getEmployees().get(i).getSurname()) && employeeIdTxt.getText().equals(restaurant.getEmployees().get(i).getId())) {
+        			logged = true;
+        		}
+        		else {
+        			Alert alert = new Alert(AlertType.ERROR);
+        	    	alert.setTitle("Log In incorrect");
+        	    	alert.setHeaderText(null);
+        	    	alert.setContentText("The username or password given are incorrect");
+        	    	alert.showAndWait();
+        	    	employeeUserTxt.setText(""); 
+        	    	employeeIdTxt.setText("");
+        		}
+        	}
+    	}
+		
+		if(logged) {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("employee.fxml"));
+			
+			fxmlLoader.setController(employeeGUI);
+			Parent EmployeeWindow = fxmlLoader.load();
+	    	
+			mainAnchorPane.getChildren().clear();
+			mainAnchorPane.getChildren().addAll(EmployeeWindow);
+		}*/
+    }	
+
     @FXML
-    void enableAdminPane(ActionEvent event) {
-
-    }
-
-    @FXML
-    void enableEmployeePane(ActionEvent event) {
-
-    }
-
-    @FXML
-    void showAdminWindow(ActionEvent event) {
-
-    }
-
-    @FXML
-    void showEmployeeWindow(ActionEvent event) {
-
+    public void showEmployeeWindow(ActionEvent event) throws IOException {
+		
     } 
     
 }
