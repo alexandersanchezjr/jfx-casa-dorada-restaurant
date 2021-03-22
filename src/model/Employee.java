363921bcabaac3b60d1a64d287c8443ba858c39c
santiagoarevalo <santiguitar2003@gmail.com>
2021-03-22 18:35:56 -0500

    Model classes update, some attributes changed and management methods implemented in Restaurant class

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -8,17 +8,22 @@ public class Employee implements Serializable {
 	private String name;
 	private String surname;
 	private String id;
+	private boolean availability;
+	private User creator;
+	private User modifier;
 	
 	/**
 	 * @param name
 	 * @param surname
 	 * @param id
 	 */
-	public Employee(String name, String surname, String id) {
-		super();
+	public Employee(String name, String surname, String id, User creator) {
 		this.name = name;
 		this.surname = surname;
 		this.id = id;
+		setAvailability(true);
+		this.creator = creator;
+		setModifier(creator);
 	}
 	
 	
@@ -43,6 +48,53 @@ public class Employee implements Serializable {
 	public void setId(String id) {
 		this.id = id;
 	}
-	
+
+
+	/**
+	 * @return the availability
+	 */
+	public boolean isAvailability() {
+		return availability;
+	}
+
+
+	/**
+	 * @param availability the availability to set
+	 */
+	public void setAvailability(boolean availability) {
+		this.availability = availability;
+	}
+
+
+	/**
+	 * @return the creator
+	 */
+	public User getCreator() {
+		return creator;
+	}
+
+
+	/**
+	 * @param creator the creator to set
+	 */
+	public void setCreator(User creator) {
+		this.creator = creator;
+	}
+
+
+	/**
+	 * @return the modifier
+	 */
+	public User getModifier() {
+		return modifier;
+	}
+
+
+	/**
+	 * @param modifier the modifier to set
+	 */
+	public void setModifier(User modifier) {
+		this.modifier = modifier;
+	}
 	
 }
--- src/model/Ingredient.java
+++ src/model/Ingredient.java
@@ -3,10 +3,14 @@ package model;
 public class Ingredient {
 	private String name;
 	private long id;
+	private User creator;
+	private User modifier;
 	
-	public Ingredient(String name, long id) {
+	public Ingredient(String name, long id, User creator) {
 		this.setName(name);
 		this.setId(id);
+		this.creator = creator;
+		setModifier(creator);
 	}
 
 	public String getName() {
@@ -30,4 +34,26 @@ public class Ingredient {
 	public void setId(long id) {
 		this.id = id;
 	}
+
+	/**
+	 * @return the creator
+	 */
+	public User getCreator() {
+		return creator;
+	}
+
+	/**
+	 * @return the modifier
+	 */
+	public User getModifier() {
+		return modifier;
+	}
+
+	/**
+	 * @param modifier the modifier to set
+	 */
+	public void setModifier(User modifier) {
+		this.modifier = modifier;
+	}
+
 }
