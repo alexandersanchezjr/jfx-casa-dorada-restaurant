157b21defe08efacbf245cf515ff1e700ff5f1b8
santiagoarevalo <santiguitar2003@gmail.com>
2021-03-24 23:00:41 -0500

    Class controller updated, new javafx attributes added

--- src/ui/AdminGUI.java
+++ src/ui/AdminGUI.java
@@ -1,5 +1,59 @@
 package ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
 public class AdminGUI {
 	
+	@FXML
+    private GridPane adminGridPane;
+	
+	@FXML
+    private Label labTime;
+
+    @FXML
+    private AnchorPane mainPane;
+    
+    @FXML
+    public void logOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void showAdminPane(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void showClientsPane(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void showEmployeePane(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void showIngredientsPane(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void showOrdersPane(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void showProductsPane(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void showTypesPane(ActionEvent event) {
+
+    }
 }
