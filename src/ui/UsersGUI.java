2d693b199b79e4a6dc8d7adcd0ff7e501ac22a19
Alexander <alexssjr2002@gmail.com>
2021-04-05 20:23:59 -0500

    newPasswordTxt type attribute has been changed as a TextField type

--- src/ui/UsersGUI.java
+++ src/ui/UsersGUI.java
@@ -96,7 +96,7 @@ public class UsersGUI {
     private PasswordField newHiddenPasswordTxt;
 
     @FXML
-    private PasswordField newPasswordTxt;
+    private TextField newPasswordTxt;
 
     @FXML
     private CheckBox chkbNewAdminShowPassword;
